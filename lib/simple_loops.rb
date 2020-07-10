064c7a045dc91b9b597288fb78b50933c3277c5b
Dean Gelbaum <deanscottg@gmail.com>
2020-07-10 18:43:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,11 @@ def loop_message_five_times (message_string)
       counter += 1 
     end 
   end
+  
+def loop_message_n_times (message, number)
+  counter = 0 
+    while counter = number do 
+      puts message 
+      counter += 1 
+    end 
+  end 
