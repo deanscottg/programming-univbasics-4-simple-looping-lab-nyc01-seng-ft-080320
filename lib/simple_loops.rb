58cfb515d1a4600f05f228e5edd2a86e87a35a5b
Dean Gelbaum <deanscottg@gmail.com>
2020-07-10 18:52:07 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,3 +21,17 @@ def loop_message_n_times (message, limit)
         count += 1 
       end 
     end 
+    
+  def return_string_array (array)
+    count = 0 
+    new_array = [] 
+    while count < length.array do 
+      new_array.push (array[count].to_s)
+        count += 1 
+      end 
+      new_array
+    end 
+      
+    
+    
+  end 
